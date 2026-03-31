4d7b06e014487a0615fad04f8e78145880ec5f0e
Juk <nunes.joao27@escola.pr.gov.br>
2026-03-31 08:34:15 -0300

    Atualização para aceitar variações de 'sim'

--- xadrez.py
+++ xadrez.py
@@ -1,5 +1,5 @@
 rafael = input("O Rafael sabe jogar xadrez? ")
-if rafael == "Sim":
+if rafael == "Sim" or "sim" or "s" or "ss" or "Ss" or "S":
     print("Para de mentir rapaz")
 else:
     print("Acertou!")
